6821e445efc408f6bae71232eb31df416cd1f0b3
EMRE <emre.bayer@hotmail.com>
2022-02-23 22:26:15 +0300

    thread function is added

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,51 @@
+
+use core::time::Duration;
+use crate::data::wifi_scan_to_string;
+
 use druid::{AppLauncher, WindowDesc,PlatformError};
 mod data;
 mod view;
 use data::AppState;
 use view::build_ui;
+use std::thread;
 
 fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(build_ui);
-    let initial_state:AppState=AppState{ name:"".into()};
-    AppLauncher::with_window(main_window).use_simple_logger()
+    let main_window = WindowDesc::new(build_ui());
+    let initial_state:AppState=AppState{ name:wifi_scan_to_string().into()};
+    let launcher=AppLauncher::with_window(main_window);
+    
+    let event_sink = launcher.get_external_handle();
+    thread::spawn(move || wifi_update(event_sink));
+
+    launcher.log_to_console()
         
         .launch(initial_state)
 }
 
 
+fn wifi_update(event_sink: druid::ExtEventSink) {
+    
+    loop {
+       
+     
+        let  data_wifi = wifi_scan_to_string();
+
+   
+      
+
+        let data_wifi_clone = data_wifi.clone();
+        
+        event_sink.add_idle_callback(move |data: &mut AppState| {
+           
+            *data =AppState { name: data_wifi_clone };
+          
+        });
+        thread::sleep(Duration::from_millis(20));
+    }
+}
 
 
 
- 
 
 
 
