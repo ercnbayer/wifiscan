30703f6452cc7829be9380193d5dbd8c4a18f332
EMRE <emre.bayer@hotmail.com>
2022-02-24 13:06:22 +0300

    Design Patern Has Been Formed

--- src/data.rs
+++ src/data.rs
@@ -1,9 +1,6 @@
 //use std::thread;
-
-
 use druid::Data;
-
-#[derive(Clone, Data,Debug)]
+#[derive(Clone, Data)]
  pub struct AppState  {
     pub name: String,
 }
@@ -23,11 +20,18 @@ pub fn wifi_scan(data:&mut Self)->&mut Self{
     data
 }
 
-}
+ /*pb fn  thread_wifi (data: &'static mut Self)->&mut Self{
+let handle =thread::spawn(move || {
+    *data=AppState{name:wifi_scan_to_string()};
+    data
+});
 
-   
-
-pub  fn wifi_scan_to_string()->String
+handle.join().unwrap()
+  
+ }*/ // work to be done here 
+ 
+} 
+pub fn _wifi_scan_to_string()->String
 {
     
        let data_wifi=wifiscanner::scan().unwrap();
@@ -39,7 +43,3 @@ pub  fn wifi_scan_to_string()->String
        string_data.push_str(" \n");
 
    }string_data}
- 
-
-
-
