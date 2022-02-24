f3d2da913f2527205b4a89a146fdf8f3aad5368e
EMRE <emre.bayer@hotmail.com>
2022-02-24 12:38:15 +0300

    Unnecessary clone  has been removed.

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,9 @@
 //use std::thread;
+
+
 use druid::Data;
-#[derive(Clone, Data)]
+
+#[derive(Clone, Data,Debug)]
  pub struct AppState  {
     pub name: String,
 }
@@ -20,18 +23,11 @@ pub fn wifi_scan(data:&mut Self)->&mut Self{
     data
 }
 
- /*pb fn  thread_wifi (data: &'static mut Self)->&mut Self{
-let handle =thread::spawn(move || {
-    *data=AppState{name:wifi_scan_to_string()};
-    data
-});
+}
 
-handle.join().unwrap()
-  
- }*/ // work to be done here 
- 
-} 
-pub fn _wifi_scan_to_string()->String
+   
+
+pub  fn wifi_scan_to_string()->String
 {
     
        let data_wifi=wifiscanner::scan().unwrap();
@@ -43,3 +39,7 @@ pub fn _wifi_scan_to_string()->String
        string_data.push_str(" \n");
 
    }string_data}
+ 
+
+
+
--- src/view.rs
+++ src/view.rs
@@ -1,27 +1,39 @@
 
+extern crate cron;
+extern crate chrono;
+
+use druid::Env;
 use druid::WidgetExt;
 use druid::widget::Flex;
 use druid::{widget::Label, Widget};
 use druid::widget::Button;
-
 use crate::data::AppState;
+
+
+
+
+
 pub fn build_ui() -> impl  Widget<AppState> {
   
     
-    let label=Label::new(|data: &AppState, _env: &_|
+    let  label=Label::dynamic(|data: &AppState, _env: &Env|format! ("{}", data.name));
+ 
+  
+    // Set minutes.
+    //cron.minutes("0");
+    // Start the cronjob.
         
-        format! ("{}", data.name));
-      
-    let button = Button::new("Scan").on_click(|_ctx, data, _env|{
     
-    AppState::wifi_scan(data);
-    
-    }).padding(5.0);
 
-    
-    Flex::column().with_child(button).with_child(label)
-}    
-    
-    
+    let button = Button::new("Scan").on_click(|_ctx, data, _env|{
       
+
+    AppState::wifi_scan(data);
+        println!("buton");
+    }).padding(5.0);
+  
+    Flex::column().with_child(button).with_child(label)
+}
+
+
         
